1bedb9e7602d7af1fad5b8836fcc12cc6cc2446a
berkereis31 <berkcark4@gmail.com>
2025-07-03 21:12:23 +0300

    Nokia 3310

---
+++ project/js/script.js
@@ -0,0 +1,12 @@
+// We will get the elements by their ID and they will look on the top and user can click them
+document.getElementById("global-header").innerHTML =  `
+  <nav>
+    <a href="index.html">Home</a>
+    <a href="about.html">About</a>
+    <a href="shop.html">Shop</a>
+    <a href="contact.html">Contact</a>
+  </nav>`;
+
+// in every html element footer will look like this on the below
+document.getElementById("global-footer").innerHTML = `
+  <footer> <p> &copy; 2025 Nokia 3310 Retro. All rights reserved. </p> </footer>`;
